b97c0e03ee3e47acdd768e7cde4739da58758d1b
zqh <870567913@qq.com>
2024-10-10 22:07:36 +0800

    2.0

--- train/asdasd.py
+++
@@ -1,9 +0,0 @@
-import torch.nn as nn
-import torch
-
-data = torch.arange(25, dtype=torch.float).reshape(5,1,5)
-
-N=2
-model = nn.ConvTranspose1d(1, 4, 2 * N, stride=N, padding=N // 2)
-
-print(model(data).shape)
